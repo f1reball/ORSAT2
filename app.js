0ea4346941711226f5bc5d43522dc0c094f2ce84
Jackson Booth <jacksontbooth@icloud.com>
2019-06-02 21:25:34 +1000

    fixed node connection for routing

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
-var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+var engines = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 const dataread = require('./test.js');
 const database_upload = require('./database_check.js');
 
 //DATABASE
 
 
-
 app.get('/', function(req, res){
-    res.sendfile("index.html");
+    res.render("./index.html");
 });
 
 app.post('/', urlencodedParser, function(req, res){
     dataread.datareader();
-    res.sendfile("index.html");
+    res.render("./index.html");
 })
 
 app.get('/test', function(req, res){
-    res.sendfile("index.html");
+    var name = 'test';
+    res.render("./index.html", {name});
 });
 
 app.post('/test', urlencodedParser, function(req, res){
@@ -29,9 +37,11 @@ app.post('/test', urlencodedParser, function(req, res){
     var last_name = req.body.last_name;
     var siid = req.body.siid;
 
+    var name = 'hello';
+
     database_upload.database_check();
     database_upload.master_running_add_new(first_name, last_name, siid);
-    res.sendfile("index.html");
+    res.render("./index.html", {name});
 });
 
 app.listen(3000, function(){
